b5d4d931cb678c3da8380ef61767894d3e9ddac3
TheOneRing <vonreth@kde.org>
2014-02-25 18:01:56 +0100

    Update main.cpp
    changed version

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,7 @@ void help()
 
 void version()
 {
-	std::wcout << L"SnoreToast version 0.4-pre" << std::endl
+	std::wcout << L"SnoreToast version 0.3" << std::endl
 		<< L"Copyright (C) 2014  Patrick von Reth <vonreth@kde.org>" << std::endl
 		<< L"SnoreToast is free software: you can redistribute it and/or modify" << std::endl
 		<< L"it under the terms of the GNU Lesser General Public License as published by" << std::endl
