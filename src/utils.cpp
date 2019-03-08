d5ce553f9c0f340cb1452417962da9df31975a72
Hannah von Reth <vonreth@kde.org>
2019-03-08 11:35:18 +0100

    Add license

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,3 +1,21 @@
+/*
+    SnoreToast is capable to invoke Windows 8 toast notifications.
+    Copyright (C) 2019  Hannah von Reth <vonreth@kde.org>
+
+    SnoreToast is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SnoreToast is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with SnoreToast.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "utils.h"
 #include "snoretoasts.h"
 
